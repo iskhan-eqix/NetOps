093e964b1786fe0aa760926163ff8ec30a94995c
iskhan-eqix <91491770+iskhan-eqix@users.noreply.github.com>
2023-02-01 16:08:21 -0800

    Create peerindb.py
    Copy script in your directory and change paths.
    Create asn_list, and put ASNs line by line
    Output will be stored in separate file in same folder

---
+++ peerindb.py
@@ -0,0 +1,30 @@
+import json
+import requests
+import pprint
+from jq import jq
+import io, sys
+
+def get_pdb_data(asn):
+    url = "https://peeringdb.com/api/net?asn=" + asn
+    resp = requests.get(url=url)
+    pdb_json = json.loads(resp.text)
+    data = jq(".data | map({name, info_prefixes4, info_prefixes6}) | .[]").transform(pdb_json)
+    return data
+
+data_list = []
+with open("asn_list.txt") as f:
+    asn_list = f.read().split()
+    fd = io.open(r'/home/iskhan/dev/peeringdb/output-pdb', 'w')
+    old_stdout = sys.stdout
+    sys.stdout = fd
+
+    for ASN in asn_list:
+     try:
+        url = "https://peeringdb.com/api/net?asn=" + ASN
+        resp = requests.get(url=url)
+        pdb_json = json.loads(resp.text)
+        data = jq(".data | map({name, asn, info_prefixes4, info_prefixes6}) | .[]").transform(pdb_json)
+        print(data)
+     except:
+        print("Error processing ASN: " + ASN)
+     continue
